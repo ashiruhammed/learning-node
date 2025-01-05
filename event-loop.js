2283b545764e35df2fd1f54b4ba1a7eae2eb609b
ashiruhammed <111865652+ashiruhammed@users.noreply.github.com>
2025-01-05 22:04:13 +0100

    Refactor event loop implementation: replace setInterval with setTimeout for better timing control

--- event-loop.js
+++ event-loop.js
@@ -1,9 +1,5 @@
 const fs = require('fs');
 
-setInterval(() => {
-  console.log('From the interval');
-}, 0);
-
 setImmediate(() => console.log('from the immediate function'));
 
 console.log('this is the top level code');

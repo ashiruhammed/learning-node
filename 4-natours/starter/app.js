dd59469e85de4a10aa8b24902935523a6d3432f3
ashiruhammed <111865652+ashiruhammed@users.noreply.github.com>
2025-01-11 23:34:01 +0100

    Add a root route to the Express server that responds with a greeting message

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,10 @@ const app = require('express')();
 
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.status(200).send('Hellow from the server side');
+});
+
 app.listen(port, () => {
   console.log('APP IS RUNNING ON PORT ${3000}');
 });

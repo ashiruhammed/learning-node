750130ae250c7854688452b33281ba2911b1a2b1
ashiruhammed <111865652+ashiruhammed@users.noreply.github.com>
2025-01-12 00:49:25 +0100

    Implement POST endpoint for adding new tours and update tours data structure in JSON file

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
-const app = require('express')();
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-
-const port = 3000;
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -14,6 +17,22 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours/', (req, res) => {
+  const newTour = Object.assign(req.body, { id: tours.length });
+  console.log(req.body, newTour);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(200).send({
+        status: 'success',
+        data: newTour,
+      });
+    }
+  );
 });
 
 app.get('/', (req, res) => {

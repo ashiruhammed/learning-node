914a8bff19c117d91bfd3d715eeae63607ab0bd5
ashiruhammed <111865652+ashiruhammed@users.noreply.github.com>
2025-01-12 01:22:58 +0100

    Add tourController with CRUD operations for tours
    - Implemented `getAllTours` to retrieve all tours from JSON file.
    - Added `getTour` to fetch a specific tour by ID with error handling for invalid IDs.
    - Created `createTour` to add new tours and update the JSON file accordingly.

---
+++ 4-natours/starter/controllers/tourController.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = parseInt(req.params.id);
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newTour = Object.assign(req.body, { id: tours.length });
+  
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'error',
+          message: 'Error writing tour data'
+        });
+      }
+      
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        },
+      });
+    }
+  );
+}; 

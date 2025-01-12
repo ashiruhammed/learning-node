09218047e467692779dd36956d3bf78ff92e7397
ashiruhammed <111865652+ashiruhammed@users.noreply.github.com>
2025-01-12 01:22:47 +0100

    Refactor app.js to integrate tour routes, add logging middleware, and implement custom request time middleware. Remove direct tour data handling and streamline error responses for undefined routes.

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,65 +1,31 @@
-const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
 
 const app = express();
-const port = 3000;
 
+// Middlewares
 app.use(express.json());
+app.use(morgan('dev')); // Add logging middleware
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+// Custom middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tours,
-    },
+// Routes
+app.use('/api/v1/tours', tourRouter);
+
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
   });
 });
 
-app.post('/api/v1/tours/', (req, res) => {
-  const newTour = Object.assign(req.body, { id: tours.length });
-  console.log(req.body, newTour);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(200).send({
-        status: 'success',
-        data: newTour,
-      });
-    }
-  );
-});
-
-app.get('/api/v1/tours/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-});
-
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Hello from the server side',
-  });
-});
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('APP IS RUNNING ON PORT ${3000}');
+  console.log(`App running on port ${port}`);
 });
